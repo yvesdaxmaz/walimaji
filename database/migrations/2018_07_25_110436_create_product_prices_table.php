b4c9c16571f574014ec106726dd0cf3404673bf4
@Tbenito <muvumbenito@gmail.com>
2018-07-25 04:28:46 -0700

    productPrice_model_migration_controller
    Creation du model ProductPrice

---
+++ database/migrations/2018_07_25_110436_create_product_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('actor_id');
+            $table->double('price_with_tax');
+            $table->double('price_without_tax');
+            $table->dateTime('date_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_prices');
+    }
+}
