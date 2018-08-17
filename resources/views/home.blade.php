c0ad51c7092edd4b5f5d09644e1a8857248cd923
bernard-ng <ngandubernard@gmail.com>
2018-08-17 17:20:00 +0200

    [UPDATE] modules.home design integrated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,9 +26,9 @@
 
 
     <div class="nav-tabs-custom map-nav-tabs">
-        <ul class="nav nav-tabs">
+        <ul class="nav nav-tabs ">
             @foreach($types as $type)
-                @if($type->designation == $tab)
+                @if($tab == $type->designation)
                     <li class="active"><a href="{{ "?tab={$type->designation}" }}">{{ $type->designation }}</a></li>
                 @else
                     <li><a href="{{ "?tab={$type->designation}" }}">{{ $type->designation }}</a></li>
@@ -38,13 +38,67 @@
     </div>
 
     <div class="content-wrapper">
-        <div class="map-container z-depth-3">
-            <iframe class="map-container-content" id="map1"
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15630.679180929914!2d27.4688104!3d-11.6468624!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x51540f372bd869d6!2sNgpictures!5e0!3m2!1sfr!2scd!4v1532932610500"
-                    frameborder="0" style="border:0" allowfullscreen="allowfullscreen"></iframe>
+        <div class="map-container">
+            <div class="map-container-content" id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3907.6698692087125!2d27.466621714831753!3d-11.646857137760964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x197239b7fd095f69%3A0x51540f372bd869d6!2sNgpictures!5e0!3m2!1sfr!2scd!4v1534326593321"
+                        width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="page-footer transparent">
+            <div class="valign-wrapper map-banner" style="background: url({{ url("images/walimaji.jpg") }});">
+                <div class="container map-banner-content">
+                    <section class="features-icons text-center">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-3">
+                                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                        <div class="features-icons-icon d-flex">
+                                            <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                        </div>
+                                        <h3>Traders</h3>
+                                        <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4
+                                            framework!</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                        <div class="features-icons-icon d-flex">
+                                            <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                        </div>
+                                        <h3>Providers</h3>
+                                        <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4
+                                            framework!</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                                        <div class="features-icons-icon d-flex">
+                                            <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                        </div>
+                                        <h3>Transfomers</h3>
+                                        <p class="lead mb-0">Ready to use with your own content, or customize the source
+                                            files!</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                                        <div class="features-icons-icon d-flex">
+                                            <i class="glyphicon glyphicon-user text-primary"></i>
+                                        </div>
+                                        <h3>Producers</h3>
+                                        <p class="lead mb-0">Ready to use with your own content, or customize the source
+                                            files!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+                <div class="map-banner-overlay"></div>
+            </div>
         </div>
     </div>
-
 
     @include('includes.default-footer')
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,13 +12,14 @@
     <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
 
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
 
     @guest
-        @include('includes.default-guest-header')
+    @include('includes.default-guest-header')
     @else
         @include('includes.default-auth-header')
     @endif
@@ -34,7 +34,8 @@
     <div class="content-wrapper">
         <div class="map-container">
             <div class="map-container-content" id="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3907.6698692087125!2d27.466621714831753!3d-11.646857137760964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x197239b7fd095f69%3A0x51540f372bd869d6!2sNgpictures!5e0!3m2!1sfr!2scd!4v1534326593321" width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3907.6698692087125!2d27.466621714831753!3d-11.646857137760964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x197239b7fd095f69%3A0x51540f372bd869d6!2sNgpictures!5e0!3m2!1sfr!2scd!4v1534326593321"
+                        width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
         <div class="container">
@@ -44,6 +45,71 @@
         </div>
     </div>
 
+
+    <div class="page-footer transparent">
+        <div class="valign-wrapper map-banner" style="background: url({{ public_path("images/walimaji.jpg") }});">
+            <div class="container map-banner-content">
+                <section class="features-icons bg-light text-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                    <div class="features-icons-icon d-flex">
+                                        <i class="icon-screen-desktop m-auto text-primary"></i>
+                                    </div>
+                                    <h3>Fully Responsive</h3>
+                                    <p class="lead mb-0">This theme will look great on any device, no matter the
+                                        size!</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                    <div class="features-icons-icon d-flex">
+                                        <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                    </div>
+                                    <h3>Bootstrap 4 Ready</h3>
+                                    <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4
+                                        framework!</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                                    <div class="features-icons-icon d-flex">
+                                        <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                    </div>
+                                    <h3>Easy to Use</h3>
+                                    <p class="lead mb-0">Ready to use with your own content, or customize the source
+                                        files!</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                                    <div class="features-icons-icon d-flex">
+                                        <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                    </div>
+                                    <h3>Easy to Use</h3>
+                                    <p class="lead mb-0">Ready to use with your own content, or customize the source
+                                        files!</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3">
+                                <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                                    <div class="features-icons-icon d-flex">
+                                        <i class="glyphicon glyphicon-user m-auto text-primary"></i>
+                                    </div>
+                                    <h3>Easy to Use</h3>
+                                    <p class="lead mb-0">Ready to use with your own content, or customize the source
+                                        files!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+            <div class="map-banner-overlay"></div>
+        </div>
+    </div>
 
     @include('includes.default-footer')
 </div>
