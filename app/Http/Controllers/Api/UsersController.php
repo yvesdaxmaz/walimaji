6224047fe14115146cb2b83cd3ff970dcc1351e1
beloti <isbelBeloti@gmail.com>
2018-08-20 16:23:30 +0200

    [UPDATE] working on user api controller

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 
 class UsersController extends Controller
 {
@@ -14,7 +19,9 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        $data = User::all();
+        return Response::create($data, 200);
+
     }
 
     /**
@@ -22,26 +29,47 @@ class UsersController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $email = $request->input('email');
+        $phone = $request->input('phone');
+        $password = Hash::make($request->input('password'));
+        $type_id = $request->input('type_id');
+
+        try {
+
+            User::firstOrCreate([
+                'name' => $name,
+                'description' => $description,
+                'email' => $email,
+                'phone' => $phone,
+                'password' => $password,
+                'type_id' => $type_id,
+                'api_token' => str_random(60)
+            ]);
+        }catch (\Exception $e){
+            return Response::create("Error value");
+        }
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -49,37 +77,32 @@ class UsersController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+
+
+        try {
+            $user = User::findOrFail($id);
+            $name = $request->input('name') ?? $user->name;
+            $description = $request->input('description') ?? $user->description;
+            $phone = $request->input('phone') ?? $user->phone;
+
+            $data = compact('name', 'description', 'email', 'phone');
+            $user->update($data);
+
+
+        } catch (ModelNotFoundException $e) {
+            return Response::create("Utilisateur n'existe plus ou pas");
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+
 }
