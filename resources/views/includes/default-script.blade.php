14cd37b8b7a99a5a3305850ab5d182ac23e71b5b
bernard-ng <ngandubernard@gmail.com>
2018-08-23 11:07:38 +0200

    create a new include file for scripts.

---
+++ resources/views/includes/default-script.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset("assets/vendor/jquery/dist/jquery.min.js") }}"></script>
+<script src="{{ asset("assets/vendor/bootstrap/dist/js/bootstrap.min.js") }}"></script>
+<script src="{{ asset("assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js") }}"></script>
+<script src="{{ asset("assets/vendor/fastclick/lib/fastclick.js") }}"></script>
+<script src="{{ asset("assets/dist/js/adminlte.min.js") }}"></script>
+<script src="{{ asset("assets/dist/js/demo.js") }}"></script>
+<script src="{{ asset("assets/js/map.js") }}"></script>
