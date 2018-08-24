4f44984fcae89faad5918599c6f56381b9c3ff23
@Tbenito <muvumbenito@gmail.com>
2018-08-24 02:33:28 -0700

    [NEW] Nofitification type migration and model created

--- database/migrations/2018_08_24_092924_create_notif_types_table.php
+++ database/migrations/2018_08_24_092924_create_notif_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class CreateNotifTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('notif_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('idSender')->references('id')->on('users');
-            $table->integer('idType')->references('id')->on('notifType');
-            $table->boolean('isRead');
+            $table->string('designation');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateNotificationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('notif_types');
     }
 }
