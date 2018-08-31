37fa41241c45038d0eeab7dee785da718b79e314
bernard-ng <ngandubernard@gmail.com>
2018-08-31 11:11:29 +0200

    [FIX] double same result from User Model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,39 +14,41 @@ use App\User;
 
 class UserController extends Controller
 {
-    public function index($id){
+    public function index($id)
+    {
 
     }
 
-    public function detail($id){
-        $details=User::getWithAdressAndType($id,'');
+    public function detail($id)
+    {
+        $user = User::findOrFail($id);
 
-        $products=Product::getWithReference($id);
+        if ($user) {
+            $details = User::getWithAdressAndType($user->id, $user->email);
+            $products = Product::getWithReference($user->id);
+            $followers = Subscription::getFollowers($user->id);
+            $following = Subscription::getFollowing($user->id);
 
-        $followers=Subscription::getFollowers($id);
+            dd($details);
 
-        $following= Subscription::getFollowing($id);
-
-        dd($details);
-
-        return view('user-detail',compact('details','products','followers','following'));
-
+            return view('user-detail', compact('details', 'products', 'followers', 'following'));
+        }
     }
 
-    public static function newSubscription($id){
-        $sub= new Subscription();
-        $sub->idsubscriber=Auth::id();
-        $sub->idActor=$id;
+    public static function newSubscription($id)
+    {
+        $sub = new Subscription();
+        $sub->idsubscriber = Auth::id();
+        $sub->idActor = $id;
         $sub->save();
     }
 
     public function store(request $request)
     {
-
         $name = $request->input('name');
         $email = $request->input('email');
         $password = Hash::make($request->input('password'));
-        $api_token=str_random(60);
+        $api_token = str_random(60);
         $type_id = $request->input('type_id');
 
         return User::create([
@@ -54,7 +56,7 @@ class UserController extends Controller
             'email' => $email,
             'password' => $password,
             'type_id' => $type_id,
-            'api_token'=>$api_token
+            'api_token' => $api_token
         ]);
 
     }
--- app/User.php
+++ app/User.php
@@ -30,65 +30,76 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public static function count(){
+    public static function count()
+    {
         return DB::table('users')
-            ->join('user_types','user_types.id','users.type_id')
+            ->join('user_types', 'user_types.id', 'users.type_id')
             ->select(DB::raw('count(users.id) as nombreUser'))
-            ->where('user_types.designation','!=','admin')->get();
+            ->where('user_types.designation', '!=', 'admin')->get();
     }
 
-    public static function getWithAdressAndType($id,$email){
+    public static function getWithAdressAndType($id, $email)
+    {
         return DB::table('users')
-            ->join('user_adresses','users.id','=','user_adresses.user_id')
-            ->join('user_types','users.type_id','=','user_types.id')
-            ->select('users.*','user_adresses.*','user_types.*')
-            ->where('users.id','=',$id,' or ','users.email','=',$email)
+            ->select('users.*', 'user_adresses.*', 'user_types.*')
+            ->join('user_adresses', 'users.id', '=', 'user_adresses.user_id')
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
+            ->where('users.id', '=', $id, ' or ', 'users.email', '=', $email)->limit(1)
             ->get();
     }
 
-    public static function getWithType(){
+    public static function getWithType()
+    {
         return DB::table('users')
-            ->join('user_types','users.type_id','=','user_types.id')
-            ->select('users.*','user_types.*')
-            ->where('users.id','=',Auth::id())
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
+            ->select('users.*', 'user_types.*')
+            ->where('users.id', '=', Auth::id())
             ->get();
     }
 
-    public static function getAllProviders(){
+    public static function getAllProviders()
+    {
         return DB::table('users')
-            ->join('user_types','users.type_id','=','user_types.id')
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
             ->select('users.*')
-            ->where('user_types.designation','=','provider')
+            ->where('user_types.designation', '=', 'provider')
             ->get();
     }
-    public static function getAllProducers(){
-        return DB::table('users')
-            ->join('user_types','users.type_id','=','user_types.id')
-            ->select('users.*')
-            ->where('user_types.designation','=','producer')
-            ->get();
-    }
-    public static function getAllTransfomers(){
-        return DB::table('users')
-            ->join('user_types','users.type_id','=','user_types.id')
-            ->select('users.*')
-            ->where('user_types.designation','=','transformer')
-            ->get();
 
-    }
-    public static function getAllTraders(){
+    public static function getAllProducers()
+    {
         return DB::table('users')
-            ->join('user_types','users.type_id','=','user_types.id')
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
             ->select('users.*')
-            ->where('user_types.designation','=','trader')
+            ->where('user_types.designation', '=', 'producer')
+            ->get();
+    }
+
+    public static function getAllTransfomers()
+    {
+        return DB::table('users')
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
+            ->select('users.*')
+            ->where('user_types.designation', '=', 'transformer')
             ->get();
 
     }
 
-    public static  function getAdminDetail(){
+    public static function getAllTraders()
+    {
         return DB::table('users')
-            ->join('user_types','users.type_id','user_types.id')
-            ->where('users.id','=',Auth::id())
-            ->select('users.name','user_types.designation')->get();
+            ->join('user_types', 'users.type_id', '=', 'user_types.id')
+            ->select('users.*')
+            ->where('user_types.designation', '=', 'trader')
+            ->get();
+
+    }
+
+    public static function getAdminDetail()
+    {
+        return DB::table('users')
+            ->join('user_types', 'users.type_id', 'user_types.id')
+            ->where('users.id', '=', Auth::id())
+            ->select('users.name', 'user_types.designation')->get();
     }
 }
