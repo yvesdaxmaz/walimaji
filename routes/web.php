5b2ee9dcfcea2382be058c4e9ca44ef2708da466
@Tbenito <muvumbenito@gmail.com>
2018-09-01 12:58:52 -0700

    [UPDATE] new admin routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('/users','Admin\AdminController@usersList')->name('users');
     Route::get('/userDetail/{id}','Admin\AdminController@userDetail')->name('userDetail');
     Route::get('/removeUser/{id}','Admin\AdminController@removeUser')->name('removeUser');
+    Route::get('/AdminProducts','Admin\AdminController@ProductsList')->name('AdminProducts');
+    Route::get('/productsRef','Admin\AdminController@productsRef')->name('productRef');
+    Route::get('/productsRefDetail/{id}','Admin\AdminController@productsRefDetail')->name('productsRefDetail');
+    Route::get('/productsType','Admin\AdminController@productsType')->name('productType');
 });
