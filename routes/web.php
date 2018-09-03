3e9da094b7f82881244c973e0298344ca2328548
@Tbenito <muvumbenito@gmail.com>
2018-09-03 01:12:30 -0700

    [ADD] add of 'Admin' prefix to admin routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ Route:: post('/check',[
 Route::get('/users/details/{id}','UserController@detail');
 
 Route::group(['middleware' => 'admin'], function () {
-    Route::get('/users','Admin\AdminController@usersList')->name('users');
-    Route::get('/userDetail/{id}','Admin\AdminController@userDetail')->name('userDetail');
-    Route::get('/removeUser/{id}','Admin\AdminController@removeUser')->name('removeUser');
-    Route::get('/AdminProducts','Admin\AdminController@ProductsList')->name('AdminProducts');
-    Route::get('/productsRef','Admin\AdminController@productsRef')->name('productRef');
-    Route::get('/productsRefDetail/{id}','Admin\AdminController@productsRefDetail')->name('productsRefDetail');
-    Route::get('/productsType','Admin\AdminController@productsType')->name('productType');
+    Route::get('/Admin_users','Admin\AdminController@usersList')->name('Admin_users');
+    Route::get('/Admin_userDetail/{id}','Admin\AdminController@userDetail')->name('Admin_userDetail');
+    Route::get('/Admin_removeUser/{id}','Admin\AdminController@removeUser')->name('Admin_removeUser');
+    Route::get('/Admin_Products','Admin\AdminController@ProductsList')->name('Admin_Products');
+    Route::get('/Admin_productsRef','Admin\AdminController@productsRef')->name('Admin_productRef');
+    Route::get('/Admin_productsRefDetail/{id}','Admin\AdminController@productsRefDetail')->name('Admin_productsRefDetail');
+    Route::get('/Admin_productsType','Admin\AdminController@productsType')->name('Admin_productType');
 });
