ca801385a184e47ee00273f822718f1f8636ccf7
@Tbenito <muvumbenito@gmail.com>
2018-09-12 16:08:12 -0700

    [ADD] table component added

---
+++ resources/views/user/subscribers.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+
+        <section class="content">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                    <div class="box box-primary ">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Vos abonnes</h3>
+
+                        </div>
+                        <div class="box-body">
+                                    <table   class="example1 table table-bordered table-striped">
+                                        <thead>
+                                        <tr>
+                                            <th>Avatar</th>
+                                            <th>Nom</th>
+                                            <th>Description</th>
+                                            <th>Type</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($followers as $follower)
+                                            <tr>
+                                                <td><img src="http://www.gstatic.com/webp/gallery/2.jpg" class="" alt="User Image" style=" height: 45px"></td>
+                                                <td>{{$follower->name}}</td>
+                                                <td> {{$follower->description}}</td>
+                                                <td> {{$follower->designation}}</td>
+                                                <td>{{$follower->email}}</td>
+                                                <td>{{$follower->phone}}</td>
+                                                <td>
+                                                    <a href="{{route('user/userDetail',$follower->id ) }}">
+                                                        <button class="btn bg-blue">Detail</button>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                        <tr>
+                                            <th>Avatar</th>
+                                            <th>Nom</th>
+                                            <th>Description</th>
+                                            <th>Type</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                    <div/>
+                    <!-- /.nav-tabs-custom -->
+                </div>
+                <div class="col-md-1"></div>
+                <!-- /.col -->
+            </div>
+        </section>
+    </div>
+@endsection
--- resources/views/user/subscriptions.blade.php
+++ resources/views/user/subscriptions.blade.php
@@ -1,7 +1,65 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: itot_pc_3
- * Date: 8/23/2018
- * Time: 3:45 PM
- */
+@extends('layouts.app')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+
+        <section class="content">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10">
+                    <div class="box box-primary ">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Vos Abonnements</h3>
+                        </div>
+                        <div class="box-body">
+                        <table   class="example1 table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Avatar</th>
+                                <th>Nom</th>
+                                <th>Description</th>
+                                <th>Type</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($followings as $following)
+                                <tr>
+                                    <td><img src="http://www.gstatic.com/webp/gallery/2.jpg" class="" alt="User Image" style=" height: 45px"></td>
+                                    <td>{{$following->name}}</td>
+                                    <td> {{$following->description}}</td>
+                                    <td> {{$following->designation}}</td>
+                                    <td>{{$following->email}}</td>
+                                    <td>{{$following->phone}}</td>
+                                    <td>
+                                        <a href="{{route('user/userDetail',$follower->id ) }}">
+                                            <button class="btn bg-blue">Se desabonner</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>Avatar</th>
+                                <th>Nom</th>
+                                <th>Description</th>
+                                <th>Type</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Action</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <div/>
+                    <!-- /.nav-tabs-custom -->
+                </div>
+                <div class="col-md-1"></div>
+                <!-- /.col -->
+            </div>
+        </section>
+    </div>
+@endsection
