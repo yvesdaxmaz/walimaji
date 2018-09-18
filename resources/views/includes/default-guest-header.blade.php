cee476c13ab00c5ea494db580c2b4a351ee32a2f
beloti <isbelBeloti@gmail.com>
2018-09-18 12:00:42 +0200

    change the color of the navbar

--- resources/views/includes/default-guest-header.blade.php
+++ resources/views/includes/default-guest-header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
     <nav class="navbar navbar-static-top">
-        <div class="container">
+        <div class="container" id="mainnav">
             <div class="navbar-header">
                 <a href="{{ url('/') }}" class="navbar-brand"><b>{{ config("APP_NAME", "Walimaji") }}</b></a>
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
